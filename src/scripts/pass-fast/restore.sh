d4e8b702154d927897803a06d6432eeba4c7c225
Gregory Haddow <gregory.haddow@ankorstore.com>
2023-03-09 11:30:01 +0000

    feat: add more commands

--- src/scripts/pass-fast/restore.sh
+++ src/scripts/pass-fast/restore.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 # Get workflows for the current pipeline and extract the previous execution of the current workflows name if there is one
 get_workflows_in_pipeline() {
-  WORKFLOW_ENDPOINT="https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID"
-  curl -f -s "$WORKFLOW_ENDPOINT" > /tmp/aks/current_wf.json
+  local WORKFLOW_ENDPOINT="https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID"
+  curl -f -s --retry 3 "$WORKFLOW_ENDPOINT" > /tmp/aks/current_wf.json
+  local CIRCLE_WORKFLOW_NAME
   CIRCLE_WORKFLOW_NAME=$(jq -r '.name ' /tmp/aks/current_wf.json)
 
-  WORKFLOWS_IN_PIPELINE_ENDPOINT="https://circleci.com/api/v2/pipeline/$CIRCLE_PIPELINE_ID/workflow?circle-token=${CIRCLE_TOKEN}"
-  curl -f -s "$WORKFLOWS_IN_PIPELINE_ENDPOINT" > /tmp/aks/pipeline_wf.json
+  local WORKFLOWS_IN_PIPELINE_ENDPOINT="https://circleci.com/api/v2/pipeline/$CIRCLE_PIPELINE_ID/workflow?circle-token=$CIRCLE_TOKEN"
+  curl -f -s --retry 3 "$WORKFLOWS_IN_PIPELINE_ENDPOINT" > /tmp/aks/pipeline_wf.json
   PREVIOUS_WORKFLOW_ID=$(jq -r --arg current_workflow_id "$CIRCLE_WORKFLOW_ID" --arg current_workflow_name "$CIRCLE_WORKFLOW_NAME" '.items[] | select(.name == $current_workflow_name and .id != $current_workflow_id).id | values' /tmp/aks/pipeline_wf.json | head -n 1)
 }
 
+# Get jobs from the previous workflow and extract the job number for this current jobs previous build.
 get_job_from_previous_workflow() {
-  JOBS_IN_WORKFLOW_ENDPOINT="https://circleci.com/api/v2/workflow/${PREVIOUS_WORKFLOW_ID}/job?circle-token=${CIRCLE_TOKEN}"
-  curl -f -s "$JOBS_IN_WORKFLOW_ENDPOINT" > /tmp/aks/previous_wf_jobs.json
-  JOB_NUM=$(jq -r --arg curjobname "$CIRCLE_JOB" '.items[] | select(.name | test($curjobname)).job_number | values' /tmp/aks/previous_wf_jobs.json)
+  local JOBS_IN_WORKFLOW_ENDPOINT="https://circleci.com/api/v2/workflow/$PREVIOUS_WORKFLOW_ID/job?circle-token=$CIRCLE_TOKEN"
+  curl -f -s --retry 3 "$JOBS_IN_WORKFLOW_ENDPOINT" > /tmp/aks/previous_wf_jobs.json
+  JOB_NUM=$(jq -r --arg current_job_name "$CIRCLE_JOB" '.items[] | select(.name | test($current_job_name)).job_number | values' /tmp/aks/previous_wf_jobs.json)
 }
 
+# Download all artifacts from a job
 get_artifacts_for_job() {
-  ARTIFACTS_URL="https://circleci.com/api/v2/project/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$JOB_NUM/artifacts?circle-token=${CIRCLE_TOKEN}"
-  curl -f -s "$ARTIFACTS_URL" > /tmp/aks/artifacts.json
+  local ARTIFACTS_URL="https://circleci.com/api/v2/project/gh/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$JOB_NUM/artifacts?circle-token=$CIRCLE_TOKEN"
+  curl -f -s --retry 3 "$ARTIFACTS_URL" > /tmp/aks/artifacts.json
+  local REQUIRED_ARTIFACTS
   REQUIRED_ARTIFACTS=$(jq -r --argjson node_index "${CIRCLE_NODE_INDEX:0}" '.items[] | select(.node_index == $node_index) | "\(.url) \(.path)"' /tmp/aks/artifacts.json)
 
   if [ -z "$REQUIRED_ARTIFACTS" ]; then
@@ -30,17 +34,32 @@ get_artifacts_for_job() {
   do
     # shellcheck disable=SC2086
     set -- $ARTIFACT
-    URL=$1
+    local URL=$1
     shift
-    FILE_PATH="$*"
+    local FILE_PATH="$*"
     echo "Downloading: $FILE_PATH"
-    curl -s -L --create-dirs -H "Circle-Token: $CIRCLE_TOKEN" -o "$FILE_PATH" "$URL"
+    curl -s -L --retry 3 --create-dirs -H "Circle-Token: $CIRCLE_TOKEN" -o "$FILE_PATH" "$URL"
   done <<< "$REQUIRED_ARTIFACTS"
 }
 
+# Has this job previously succeeded in this pipeline?
 PASS_RECORD=$(echo "$CIRCLE_JOB-$CIRCLE_NODE_INDEX-$CIRCLE_PIPELINE_ID" | sed -e "s/[^[:alnum:]]/-/g" | tr -s "-" | tr "[:upper:]" "[:lower:]")
 if [ -f ".pass/$PASS_RECORD" ]; then
+
+  # Check dependencies
+  if [ -z "$BASH" ]; then
+    echo "Bash not installed. Aborting."
+    exit 1
+  fi
+  hash jq 2>/dev/null || { echo >&2 "jq is not installed. Aborting."; exit 1; }
+  hash curl 2>/dev/null || { echo >&2 "curl is not installed. Aborting."; exit 1; }
+  if [ -z "$CIRCLE_TOKEN" ]; then
+    echo "CIRCLE_TOKEN env not set. Please set a valid CircleCi API Token in the env var CIRCLE_TOKEN";
+    exit 1;
+  fi
+
   echo "This job has succeeded previously in this workflow restoring artifacts..."
+  # export this to env for the later skip script
   echo 'export PASS_FAST="true"' >> "$BASH_ENV"
   mkdir -p /tmp/aks
 
@@ -51,7 +70,7 @@ if [ -f ".pass/$PASS_RECORD" ]; then
     get_job_from_previous_workflow
     if [ -n "$JOB_NUM" ]; then
       echo "Getting artifacts from previous job: $JOB_NUM"
-      cd "$CIRCLE_WORKING_DIRECTORY" || exit
+      cd "$CIRCLE_WORKING_DIRECTORY" || exit 1
       get_artifacts_for_job
     else
        echo "No previous execution of this job found in the workflow $PREVIOUS_WORKFLOW_ID"
@@ -62,4 +81,5 @@ if [ -f ".pass/$PASS_RECORD" ]; then
 else
   echo "Job has not previously succeeded, no artifacts to restore"
 fi
+# cleanup any pass records
 rm -rf .pass
---
+++ src/scripts/wait-for-job.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+if [ -z "$BASH" ]; then
+  echo "Bash not installed. Aborting."
+  exit 1
+fi
+hash jq 2>/dev/null || { echo >&2 "jq is not installed. Aborting."; exit 1; }
+hash curl 2>/dev/null || { echo >&2 "curl is not installed. Aborting."; exit 1; }
+if [ -z "$CIRCLE_TOKEN" ]; then
+  echo "CIRCLE_TOKEN env not set. Please set a valid CircleCi API Token in the env var CIRCLE_TOKEN";
+  exit 1;
+fi
+
+mkdir -p /tmp/aks
+
+get_job_status() {
+  local NAME_OF_JOB="$1"
+  local STATUS=""
+  local NUMBER=""
+  local WORKFLOW_JOBS_URL="https://circleci.com/api/v2/workflow/$CIRCLE_WORKFLOW_ID/job?circle-token=$CIRCLE_TOKEN"
+  local WORKFLOW_JOBS_JSON="/tmp/aks/wf_$CIRCLE_WORKFLOW_ID.json"
+  curl  -f -s --retry 3 "$WORKFLOW_JOBS_URL" > "$WORKFLOW_JOBS_JSON"
+  STATUS=$(jq -r ".items[] | select(.name==\"$NAME_OF_JOB\") | .status | values" "$WORKFLOW_JOBS_JSON")
+  NUMBER=$(jq -r ".items[] | select(.name==\"$NAME_OF_JOB\") | .JOB_NUMBER | values" "$WORKFLOW_JOBS_JSON")
+  echo "$STATUS $NUMBER"
+}
+
+echo "Waiting for job(s): $JOB_LIST"
+echo "Max wait: $MAX_WAIT_TIME"
+
+for JOB_NAME in ${JOB_LIST//,/ }
+do
+  # Reset job status, job number and wait time
+  JOB_STATUS=""
+  JOB_NUMBER=""
+  CURRENT_WAIT_TIME=0
+
+  echo "Starting to check status of $JOB_NAME"
+  while true; do
+    read -r JOB_STATUS JOB_NUMBER < <(get_job_status "$JOB_NAME")
+    if [ -z "$JOB_NUMBER" ]; then
+      echo "No job with name $JOB_NAME can be found in this workflow"
+      break
+    fi
+    if [[ "$JOB_STATUS" != "queued" && "$JOB_STATUS" != "running" && "$JOB_STATUS" != "not_running" ]]; then
+      echo "Job $JOB_NAME has completed! Status: $JOB_STATUS"
+      if [ "$JOB_STATUS" != "success" ] && $CHECK_FOR_FAILURE; then
+        echo "The $JOB_NAME job failed, failing this job.";
+        exit 1;
+      fi
+      break
+    else
+      echo "Looks like $JOB_NAME($JOB_NUMBER) is still not done. Status: $JOB_STATUS"
+      echo "Going to sleep for $SLEEP_TIME"
+      sleep "$SLEEP_TIME"
+      CURRENT_WAIT_TIME=$(( CURRENT_WAIT_TIME + SLEEP_TIME ))
+    fi
+
+    if (( CURRENT_WAIT_TIME > MAX_WAIT_TIME )); then
+      if $CONTINUE_ON_TIMEOUT; then
+        echo "Max wait timout reached! Proceeding with further steps";
+        break
+      else
+        echo "Max wait timout reached! Failing job!";
+        exit 1;
+      fi
+    fi
+  done
+done
