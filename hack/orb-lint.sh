1ea1b6d6cc26f383f0d0678cb21456e22fbe4190
Gregory Haddow <93638800+ankorstore-haddowg@users.noreply.github.com>
2023-03-14 12:51:09 +0000

    Feat: add sonar-scan command (#18)
    * chore: add orb review checks to pre-commit
    
    * chore: add orb pack and validate check to pre-commit
    
    * feat: add sonar-scan command

---
+++ hack/orb-lint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if ! command -v bats >/dev/null; then
+	echo 'The "bats-core" automation framework must be installed.'
+	echo 'See https://bats-core.readthedocs.io/en/stable/installation.html'
+	exit 1
+fi
+if ! command -v yq >/dev/null; then
+	echo 'The "yq" package must be installed.'
+	exit 1
+fi
+curl -f -s -o ./hack/orb.bats https://raw.githubusercontent.com/CircleCI-Public/orb-tools-orb/master/src/scripts/review.bats
+PARAM_MAX_COMMAND_LENGTH=64 bats ./hack/orb.bats
