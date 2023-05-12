4857cbb234ea84d3b7bf63ce444bfe44fb8c20c8
Gregory Haddow <gregory.haddow@ankorstore.com>
2023-05-12 08:57:43 +0100

    fix: fix bats linter script

--- hack/orb-lint.sh
+++ hack/orb-lint.sh
@@ -8,6 +8,5 @@ if ! command -v yq >/dev/null; then
 	echo 'The "yq" package must be installed.'
 	exit 1
 fi
-# use modified local rules until https://github.com/CircleCI-Public/orb-tools-orb/pull/196 this issue is fixed
-#curl -f -s -o ./hack/orb.bats https://raw.githubusercontent.com/CircleCI-Public/orb-tools-orb/master/src/scripts/review.bats
-PARAM_RC_EXCLUDE=RC006 PARAM_MAX_COMMAND_LENGTH=120 bats ./hack/orb.bats
+curl -f -s -o ./hack/orb.bats https://raw.githubusercontent.com/CircleCI-Public/orb-tools-orb/master/src/scripts/review.bats
+ORB_VAL_RC_EXCLUDE=RC010 ORB_VAL_MAX_COMMAND_LENGTH=120 bats ./hack/orb.bats
