f4fed9ffdead7ec9f6b0fcf314000d7a96428cea
Gregory Haddow <gregory.haddow@ankorstore.com>
2023-07-28 11:42:56 +0100

    chore: improve store_compressed_artifacts command output

--- src/scripts/store_compressed_artifacts.sh
+++ src/scripts/store_compressed_artifacts.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 mkdir "/tmp/$TARBALL_NAME"
+
+echo "Creating tarball of $ARTIFACT_PATH"
+echo " => /tmp/$TARBALL_NAME/$TARBALL_NAME-$CIRCLE_NODE_INDEX.tar.gz"
+
+ls -lah "$ARTIFACT_PATH"
+
 tar -czf "/tmp/$TARBALL_NAME/$TARBALL_NAME-$CIRCLE_NODE_INDEX.tar.gz" "$ARTIFACT_PATH"
