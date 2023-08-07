191b6fa9fbedc6bf83bb83a53db333c23e5cc5ce
Gregory Haddow <93638800+ankorstore-haddowg@users.noreply.github.com>
2023-08-07 13:03:41 +0100

    feat: new commands (#46)
    * ci: add pr checks
    
    * chore: improve store_compressed_artifacts command output
    
    * feat: add get_pr_info and post_pr_comment commands
    
    * feat: add get_test_timings and save_test_timings commands

---
+++ src/scripts/post_pr_comment.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if ! (command -v jq >/dev/null 2>&1); then
+  echo "This command requires jq to be installed"
+  exit 1
+fi
+
+if [ -z "$CIRCLE_BRANCH" ]; then
+  echo "No Branch detected, tag pipelines will not have a PR."
+  exit 0
+fi
+
+# Get the associated PR number
+PR_NUMBER=$(gh pr list --head "$CIRCLE_BRANCH" --json number -q '.[] | .number')
+
+if [ -z "$PR_NUMBER" ]; then
+  echo "No associated PR"
+  exit "$FAIL_WITHOUT_PR"
+fi
+
+echo "PR_NUMBER: $PR_NUMBER"
+FULL_COMMENT_TAG=""
+COMMENT_ID=""
+if [ -n "$COMMENT_FILE" ] && [ -f "$COMMENT_FILE" ]; then
+  COMMENT=$(cat "$COMMENT_FILE")
+fi
+echo "$COMMENT" > body.md
+
+if [ -n "$COMMENT_TAG" ]; then
+  echo "COMMENT_TAG: $COMMENT_TAG"
+  FULL_COMMENT_TAG="<!-- toolbelt/post_pr_comment ${COMMENT_TAG} -->"
+  COMMENT_ID=$(gh api "/repos/{owner}/{repo}/issues/$PR_NUMBER/comments" --jq ".[] | select(.body | contains(\"$FULL_COMMENT_TAG\")) | .id")
+  printf "\n%s" "$FULL_COMMENT_TAG" >> body.md
+fi
+
+if [ -z "${COMMENT_ID}" ]; then
+  if [ -n "$COMMENT_TAG" ]; then
+    echo "No comment tagged \`$COMMENT_TAG\` found, creating new comment"
+  else
+    echo "Creating new comment"
+  fi
+  gh pr comment --body-file body.md
+else
+  echo "Updating comment"
+  echo "COMMENT_ID: $COMMENT_ID"
+  gh api "/repos/{owner}/{repo}/issues/comments/$COMMENT_ID" -X PATCH -f body="$(cat body.md)"
+fi
